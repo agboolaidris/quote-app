6169ec556ae3a66f9c7d7bbd2a367c02d2bab804
Iris <agboolaisholaidreez>
2023-08-09 18:15:58 +0100

    update

--- app/(tabs)/authors/_layout.tsx
+++ app/(tabs)/authors/_layout.tsx
@@ -1,60 +1,6 @@
-import { COLORS, FONT, SIZES } from "@/constants";
+import { Header } from "@/components/commons/Header";
+import { COLORS, SIZES } from "@/constants";
 import { Stack } from "expo-router";
-import { Text, View, SafeAreaView, Platform, StatusBar } from "react-native";
-import { NativeStackHeaderProps } from "@react-navigation/native-stack";
-import { ArrowBackIcon } from "@/components/icons/ArrowBack";
-import { AnimatePressable } from "@/components/commons/AnimatePressable";
-
-const Header = ({ navigation, options }: NativeStackHeaderProps) => {
-  const isBack = navigation.canGoBack();
-  const goBack = () => {
-    if (isBack) {
-      navigation.goBack();
-    }
-  };
-  return (
-    <SafeAreaView
-      style={{
-        backgroundColor: COLORS.gray[700],
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-      }}
-    >
-      <StatusBar backgroundColor={COLORS.gray[700]} barStyle="light-content" />
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          position: "relative",
-        }}
-      >
-        {isBack && (
-          <AnimatePressable
-            onPress={goBack}
-            style={{
-              position: "absolute",
-              zIndex: 10,
-              left: 5,
-            }}
-          >
-            <ArrowBackIcon width={30} height={30} color={COLORS.gray[50]} />
-          </AnimatePressable>
-        )}
-        <View style={{ flex: 1, paddingVertical: 10 }}>
-          <Text
-            style={{
-              textAlign: "center",
-              fontSize: SIZES.xxl,
-              color: COLORS.gray[50],
-              fontWeight: "bold",
-            }}
-          >
-            {options.headerTitle?.toString()}
-          </Text>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
-};
 
 export default function SaveLayout() {
   return (
@@ -68,6 +14,7 @@ export default function SaveLayout() {
         headerTitle: "Quotes.",
         headerStyle: { backgroundColor: COLORS.gray[50] },
         header: Header,
+        headerShown: false,
       }}
     >
       <Stack.Screen name="index" options={{ headerTitle: "Authors." }} />
--- components/commons/Header.tsx
+++ components/commons/Header.tsx
@@ -1,25 +1,111 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Animated,
+} from "react-native";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack";
+import { ArrowBackIcon } from "@/components/icons/ArrowBack";
+import { AnimatePressable } from "@/components/commons/AnimatePressable";
 import { COLORS, SIZES } from "@/constants";
+import { NavigationState, SceneRendererProps } from "react-native-tab-view";
 
-type HeaderProps = {
-  title?: string;
-};
-
-export const Header = ({ title }: HeaderProps) => {
+export const Header = ({ navigation, options }: NativeStackHeaderProps) => {
+  const isBack = navigation.canGoBack();
+  const goBack = () => {
+    if (isBack) {
+      navigation.goBack();
+    }
+  };
   return (
-    <View
-      style={{ height: 50, justifyContent: "center", alignItems: "center" }}
+    <SafeAreaView
+      style={{
+        backgroundColor: COLORS.gray[700],
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      }}
     >
-      <Text
+      <StatusBar backgroundColor={COLORS.gray[700]} barStyle="light-content" />
+      <View
         style={{
-          fontSize: SIZES.lg,
-          color: COLORS.gray[600],
-          fontWeight: "bold",
+          flexDirection: "row",
+          alignItems: "center",
+          position: "relative",
         }}
       >
-        {title || "Quote"}.
-      </Text>
-    </View>
+        {isBack && (
+          <AnimatePressable
+            onPress={goBack}
+            style={{
+              position: "absolute",
+              zIndex: 10,
+              left: 5,
+            }}
+          >
+            <ArrowBackIcon width={30} height={30} color={COLORS.gray[50]} />
+          </AnimatePressable>
+        )}
+        <View style={{ flex: 1, paddingVertical: 5 }}>
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: SIZES.lg,
+              color: COLORS.gray[50],
+              fontWeight: "bold",
+            }}
+          >
+            {options.headerTitle?.toString()}
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+type RenderTabBarProps = SceneRendererProps & {
+  navigationState: NavigationState<{
+    key: string;
+    title: string;
+  }>;
+  setIndex: any;
+};
+
+export const TopTabBar = (props: RenderTabBarProps) => {
+  return (
+    <SafeAreaView
+      style={{
+        flexDirection: "row",
+        backgroundColor: COLORS.gray[700],
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      }}
+    >
+      {props.navigationState.routes.map((route, i) => {
+        return (
+          <AnimatePressable
+            onPress={() => props.setIndex(i)}
+            style={{
+              flex: 1,
+              borderBottomColor: COLORS.gray[300],
+              borderBottomWidth: props.navigationState.index === i ? 3 : 0,
+            }}
+            key={i}
+          >
+            <Animated.Text
+              style={{
+                textAlign: "center",
+                paddingVertical: 5,
+                color:
+                  props.navigationState.index === i
+                    ? COLORS.gray[300]
+                    : COLORS.gray[500],
+              }}
+            >
+              {route.title}
+            </Animated.Text>
+          </AnimatePressable>
+        );
+      })}
+    </SafeAreaView>
   );
 };
